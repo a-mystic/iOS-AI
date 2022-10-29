bce5c0e00dd345d4be90ad044444c78064a146d9
a-mystic <totocaca@naver.com>
2022-10-29 17:00:32 +0900

    .

---
+++ SwiftUI AVFoundation AI/ObjectRecognition/ObjectRecognition/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  ObjectRecognition
+//
+//  Created by mystic on 2022/10/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HostedViewController()
+            .ignoresSafeArea()
+    }
+}
---
+++ SwiftUI AVFoundation AI/ObjectRecognition/ObjectRecognition/ObjectRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObjectRecognitionApp.swift
+//  ObjectRecognition
+//
+//  Created by mystic on 2022/10/29.
+//
+
+import SwiftUI
+
+@main
+struct ObjectRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
